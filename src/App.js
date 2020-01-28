b4b88e33ed00e282663560433879e91fecde4274
Oretastic <hloreair@outlook.com>
2020-01-27 19:40:28 -0500

    added utilities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import qs from 'qs'
+import { getHash } from './utilities/utils'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import Users from './components/Users'
+
+const API = `https://acme-users-api-rev.herokuapp.com/`
 
 function App() {
-
-  const API = `https://acme-users-api-rev.herokuapp.com/`
-  
-  const getHash = ()=> {
-    return window.location.hash.slice(1);
-  }
 
   const [ params, setParams ] = useState(qs.parse(getHash()));
 
@@ -20,7 +20,9 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Nav />
+      {params.view === undefined ? <Home />: null}
+      {params.view === 'users' ? <Users />: null}
     </div>
   );
 }
